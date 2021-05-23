e433cf1f576ae5e202c4d37f71346139efba787c
Pablo Cornejo <pcornejo@icloud.com>
2021-05-23 19:15:25 -0400

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Pablo Cornejo on 5/23/21.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
