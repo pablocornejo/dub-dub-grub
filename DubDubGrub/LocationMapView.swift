428ce6147c12bf680a130e15b6a67047f6320b66
Pablo Cornejo <pcornejo@icloud.com>
2021-05-23 19:49:05 -0400

    Add basic map UI

--- DubDubGrub/LocationMapView.swift
+++ DubDubGrub/LocationMapView.swift
@@ -6,10 +6,23 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationMapView: View {
+    @State private var region = MKCoordinateRegion(center: .init(latitude: 37.331516, longitude: -121.891054),
+                                                   span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    
     var body: some View {
-        Text("Map View")
+        ZStack(alignment: .top) {
+            Map(coordinateRegion: $region)
+                .ignoresSafeArea()
+            
+            Image("ddg-map-logo")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 70)
+                .shadow(radius: 10)
+        }
     }
 }
 
