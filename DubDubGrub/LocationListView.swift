ce4bb604755bc2fb2f8fb43ae1eb4880a8dd05a4
Pablo Cornejo <pcornejo@icloud.com>
2021-05-24 21:43:13 -0400

    Add navigation link to location list view cells

--- DubDubGrub/LocationListView.swift
+++ DubDubGrub/LocationListView.swift
@@ -12,29 +12,8 @@ struct LocationListView: View {
         NavigationView {
             List {
                 ForEach(0..<10) { index in
-                    HStack {
-                        Image("default-square-asset")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 80, height: 80)
-                            .clipShape(Circle())
-                            .padding(.vertical, 8)
-                        
-                        VStack(alignment: .leading) {
-                            Text("Test location name")
-                                .font(.title2)
-                                .fontWeight(.semibold)
-                                .lineLimit(1)
-                                .minimumScaleFactor(0.8)
-                            
-                            HStack {
-                                AvatarView(size: 36)
-                                AvatarView(size: 36)
-                                AvatarView(size: 36)
-                                AvatarView(size: 36)
-                            }
-                        }
-                        .padding(.leading)
+                    NavigationLink(destination: LocationDetailView()) {
+                        LocationCell()
                     }
                 }
             }
@@ -46,6 +25,7 @@ struct LocationListView: View {
 struct LocationListView_Previews: PreviewProvider {
     static var previews: some View {
         LocationListView()
+            .preferredColorScheme(.dark)
     }
 }
 
@@ -58,5 +38,34 @@ struct AvatarView: View {
             .scaledToFit()
             .frame(width: size, height: size)
             .clipShape(Circle())
+    }
+}
+
+struct LocationCell: View {
+    var body: some View {
+        HStack {
+            Image("default-square-asset")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80)
+                .clipShape(Circle())
+                .padding(.vertical, 8)
+            
+            VStack(alignment: .leading) {
+                Text("Test location name")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.8)
+                
+                HStack {
+                    AvatarView(size: 36)
+                    AvatarView(size: 36)
+                    AvatarView(size: 36)
+                    AvatarView(size: 36)
+                }
+            }
+            .padding(.leading)
+        }
     }
 }
